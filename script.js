025bb9b4cedd7e2a627620c479cb35d0b1dbde6a
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-17 11:59:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -56,5 +56,40 @@ function get(){
   document.getElementById("staff-4").value = values.staff_4
   document.getElementById("staff-5").value = values.staff_5
   document.getElementById("staff-6").value = values.staff_6
+
 }
 get()
+
+
+function graph(){
+
+var values = JSON.parse(localStorage.getItem("values"))
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	exportEnabled: true,
+	animationEnabled: true,
+	title: {
+		text: "Last Amount Rates"
+	},
+	data: [{
+		type: "pie",
+		startAngle: 180,
+		toolTipContent: "<b>{label}</b>: {y}%",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 11,
+		indexLabel: "{label} - {y}%",
+		dataPoints: [
+			{ y: values.staff_1, label: "Staff-1" },
+			{ y: values.staff_2, label: "Staff-2" },
+			{ y: values.staff_3, label: "Staff-3" },
+			{ y: values.staff_4, label: "Staff-4" },
+			{ y: values.staff_5, label: "Staff-5" },
+			{ y: values.staff_6, label: "Staff-6" }
+		]
+	}]
+});
+chart.render();
+
+
+}
